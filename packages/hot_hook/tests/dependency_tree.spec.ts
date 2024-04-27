Julien-R44/hot-hook

commit 117f9c150df9b5ec1463f59364a27572c7f3f3e8

fix: do not increase version param if file is not hot reloadable

packages/hot_hook/src/dependency_tree.ts
@@ -90,6 +90,7 @@ export default class DependencyTree {
 
     childNode.parents?.add(parentNode)
     parentNode.dependencies.add(childNode)
+    this.addDependent(parentNode.path, childNode.path)
   }
 
   /**
@@ -116,6 +117,7 @@ export default class DependencyTree {
       if (!invalidatedFiles.has(currentPath)) {
         const node = this.#pathMap.get(currentPath)
         if (!node) continue
+        if (!this.isReloadable(currentPath)) continue
 
         node.version++
         invalidatedFiles.add(currentPath)

packages/hot_hook/tests/dependency_tree.spec.ts
@@ -30,4 +30,26 @@ test.group('Dependency tree', () => {
     assert.deepEqual(tree.isReloadable('models/post.ts'), true)
     assert.deepEqual(tree.isReloadable('services/post_service.ts'), true)
   })
+
+  test('should not increase the version of a file if it is not reloadable', ({ assert }) => {
+    const tree = new DependencyTree({ root: 'app.ts' })
+
+    tree.addDependency('app.ts', { path: 'start/index.ts' })
+    tree.addDependency('start/index.ts', { path: 'config/auth.ts' })
+    tree.addDependency('config/auth.ts', { path: 'models/user.ts' })
+    tree.addDependency('start/index.ts', {
+      reloadable: true,
+      path: 'controllers/users_controller.ts',
+    })
+
+    tree.addDependency('controllers/users_controller.ts', { path: 'models/user.ts' })
+
+    assert.isFalse(tree.isReloadable('models/user.ts'))
+    assert.deepEqual(tree.getVersion('models/user.ts'), 0)
+
+    const invalidatedFiles = tree.invalidateFileAndDependents('controllers/users_controller.ts')
+
+    assert.notInclude(Array.from(invalidatedFiles), 'models/user.ts')
+    assert.deepEqual(tree.getVersion('models/user.ts'), 0)
+  })
 })

packages/hot_hook/src/loader.ts
@@ -175,7 +175,6 @@ export class HotHookLoader {
       const reloadable = context.importAttributes.hot === 'true' ? true : isHardcodedBoundary
 
       this.#dependencyTree.addDependency(parentPath, { path: resultPath, reloadable })
-      this.#dependencyTree.addDependent(resultPath, parentPath)
     }
 
     if (this.#pathIgnoredMatcher.match(resultPath)) {

commit 07cadde5e3c37309394967070063d635fbd397d7

fix: incorrect graph dependent addition introduced in 117f9c150df9b5ec1463f59364a27572c7f3f3e8

packages/hot_hook/src/dependency_tree.ts
@@ -90,7 +90,7 @@ export default class DependencyTree {
 
     childNode.parents?.add(parentNode)
     parentNode.dependencies.add(childNode)
-    this.addDependent(parentNode.path, childNode.path)
+    this.addDependent(dependency.path, parentPath)
   }
 
   /**

pnpm-lock.yaml
@@ -118,8 +118,8 @@ importers:
         specifier: ^8.57.0
         version: 8.57.0
       hot-hook:
-        specifier: ^0.2.1
-        version: 0.2.2
+        specifier: workspace:*
+        version: link:../../packages/hot_hook
       pino-pretty:
         specifier: ^11.0.0
         version: 11.0.0
@@ -2750,9 +2750,6 @@ packages:
   hosted-git-info@7.0.1:
     resolution: {integrity: sha512-+K84LB1DYwMHoHSgaOY/Jfhw3ucPmSET5v98Ke/HdNSw4a0UktWzyW1mjhjpuxxTqOOsfWT/7iVshHmVZ4IpOA==}
     engines: {node: ^16.14.0 || >=18.0.0}
-
-  hot-hook@0.2.2:
-    resolution: {integrity: sha512-CwXbOcTFc91m5CyGNp39tSe3yj/Bl4q9YfFwypSMJl6PpS3vXsbOdKtGLNNXHOIGFHDj5UpJp1Pu72/OE9UPaQ==}
 
   html-escaper@2.0.2:
     resolution: {integrity: sha512-H2iMtd0I4Mt5eYiapRdIDjp+XzelXQ0tFE4JS7YFwFevXXMmOp9myNrUvCg0D6ws8iqkRPBfKHgbwig1SmlLfg==}
@@ -7484,13 +7481,6 @@ snapshots:
   hosted-git-info@7.0.1:
     dependencies:
       lru-cache: 10.2.0
-
-  hot-hook@0.2.2:
-    dependencies:
-      chokidar: 3.6.0
-      fast-glob: 3.3.2
-      picomatch: 4.0.2
-      read-package-up: 11.0.0
 
   html-escaper@2.0.2: {}
 

packages/hot_hook/tests/dependency_tree.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@japa/runner'
 import DependencyTree from '../src/dependency_tree.js'
 
 test.group('Dependency tree', () => {
-  test('basic scenario', ({ assert }) => {
+  test('scenario 1', ({ assert }) => {
     const tree = new DependencyTree({ root: 'app.ts' })
 
     tree.addDependency('app.ts', { path: 'start/index.ts' })
@@ -29,6 +29,34 @@ test.group('Dependency tree', () => {
     assert.deepEqual(tree.isReloadable('models/user.ts'), false)
     assert.deepEqual(tree.isReloadable('models/post.ts'), true)
     assert.deepEqual(tree.isReloadable('services/post_service.ts'), true)
+  })
+
+  test('scenario 2', ({ assert }) => {
+    const tree = new DependencyTree({ root: 'app.ts' })
+
+    tree.addDependency('app.ts', { path: 'start/index.ts' })
+    tree.addDependency('app.ts', { path: 'models/user.ts' })
+    tree.addDependency('start/index.ts', {
+      path: 'controllers/users_controller.ts',
+      reloadable: true,
+    })
+    tree.addDependency('controllers/users_controller.ts', { path: 'models/user.ts' })
+    tree.addDependency('controllers/users_controller.ts', { path: 'user_presenter.ts' })
+    tree.addDependency('user_presenter.ts', { path: 'utils/index.ts' })
+
+    assert.deepEqual(tree.isReloadable('app.ts'), false)
+    assert.deepEqual(tree.isReloadable('models/user.ts'), false)
+    assert.deepEqual(tree.isReloadable('start/index.ts'), false)
+    assert.deepEqual(tree.isReloadable('controllers/users_controller.ts'), true)
+    assert.deepEqual(tree.isReloadable('user_presenter.ts'), true)
+
+    const invalidated = tree.invalidateFileAndDependents('user_presenter.ts')
+
+    assert.deepEqual(invalidated.size, 2)
+    assert.deepEqual(tree.getVersion('user_presenter.ts'), 1)
+    assert.deepEqual(tree.getVersion('controllers/users_controller.ts'), 1)
+    assert.deepEqual(tree.getVersion('models/user.ts'), 0)
+    assert.deepEqual(tree.getVersion('utils/index.ts'), 0)
   })
 
   test('should not increase the version of a file if it is not reloadable', ({ assert }) => {
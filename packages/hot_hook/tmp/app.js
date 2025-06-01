216d0cbd2bd792913ec4f4adedf351782c6f2001
Julien Ripouteau <julien@ripouteau.com>
2025-06-01 11:17:57 +0200

    chore: remove tmp files

--- packages/hot_hook/tmp/app.js
+++
@@ -1,4 +0,0 @@
-export default function(request, response) {
-      response.writeHead(200, {'Content-Type': 'text/plain'})
-      response.end('Hello World!')
-    }
--- packages/hot_hook/tmp/server.js
+++
@@ -1,9 +0,0 @@
-import * as http from 'http'
-       import { join } from 'node:path'
-
-       const server = http.createServer(async (request, response) => {
-         const app = await import('./app.js')
-         await app.default(request, response)
-       })
-
-       server.listen(3333, () => console.log('Server is running'))
